a401f0a6881c4a61814392c3a913161a7b774695
chinmaya <cthejasvius001@gmail.com>
2026-08-19 19:51:54 +0530

    Updated App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,1 +1,9 @@
-
+package demo
+public class App{
+  public static String getMessage(){
+     return "hello";
+       }
+  public static void main(String[] args){
+    System.out.println(getMessage());
+  }
+}
